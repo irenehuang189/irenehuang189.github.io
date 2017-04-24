a731484521dd41aeb575b1b7dded176edb069539
irenehuang189 <irenehuang189@gmail.com>
2017-04-24 13:05:50 +0700

    Add notification in menu

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,4 +4,19 @@ $(document).ready(function(){
   $('.post-detail').click(function(){
     $('.ui.modal').modal('show');
   });
+
+  $('.menu #notification').popup({
+    on: 'click',
+    lastResort: 'bottom right',
+    onShow: function(){
+            resizePopup();
+        },
+  });
+
+  // Function for resizing popup
+  var resizePopup = function(){$('.ui.popup').css('max-height', $(window).height());};
+
+  $(window).resize(function(e){
+      resizePopup();
+  });
 });
